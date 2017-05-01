dedbbe371966b1dacdbe6a504840f587732f77e3
riskteria <riskteria@gmail.com>
2017-05-01 18:02:57 +0700

    initializing

---
+++ src/styles/_colors.js
@@ -0,0 +1,4 @@
+export const colorAccent = '#1ba39c'
+export const colorDark = '#4d4d4d'
+export const colorGrey = '#95989a'
+export const colorLight = '#FFFFFF'
---
+++ src/styles/_dimension.js
@@ -0,0 +1,11 @@
+// Padding
+export const leftPadding = 16
+export const rightPadding = 16
+export const topPadding = 16
+export const bottomPadding = 16
+
+// Margin
+export const topMargin = 8
+export const bottomMargin = 8
+export const leftMargin = 8
+export const rightMargin = 8
