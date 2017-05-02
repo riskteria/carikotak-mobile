a719bb69d9925a921849636521995346c4e54f58
riskteria <riskteria@gmail.com>
2017-05-02 14:53:39 +0700

    fix screen

--- src/app.android.js
+++ src/app.android.js
@@ -1,3 +1,16 @@
-import screens from './screens';
+import { AppRegistry } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default screens;
+import LandingScreen from './modules/views/LandingScreen';
+import LoginScreen from './modules/views/LoginScreen';
+
+const CarikotakApp = StackNavigator({
+	Home: {
+		screen: LandingScreen
+	},
+	Login: {
+		screen: LoginScreen
+	}
+});
+
+AppRegistry.registerComponent('carikotak', () => CarikotakApp);
