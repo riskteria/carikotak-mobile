e52adc91bce48d23b0609f8ccda9e15164c9880e
riskteria <riskteria@gmail.com>
2017-05-09 00:28:10 +0700

    add search on right nav

--- src/components/feed/FeedScreen.js
+++ src/components/feed/FeedScreen.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button, Icon } from 'native-base';
 
 import styles from './styles';
 
 class FeedScreen extends Component {
+
+	static navigationOptions = ({ navigation }) => {
+		return {
+			headerTintColor: '#000',
+			headerRight: (
+				<Button light transparent onPress={() => {}}>
+					<Icon style={{ color: '#000' }} name="ios-search-outline" />
+				</Button>
+			)
+		};
+	}
 
 	render () {
 		return (
