2babac62aff90cb0e26afc61c91f61dae84e9179
riskteria <riskteria@gmail.com>
2017-05-09 00:00:30 +0700

    bottom tab bar icons

--- src/configs/navigators.js
+++ src/configs/navigators.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Icon } from 'native-base';
+
 import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import LandingScreen from '../components/landing/LandingScreen';
@@ -16,43 +19,68 @@ const MainNavigator = TabNavigator({
 	Feed: {
 		screen: FeedScreen,
 		navigationOptions: {
+			tabBarLabel: 'Beranda',
+			tabBarIcon: ({ tintColor }) => (
+				<Icon style={{ color: tintColor }} name="ios-home-outline" />
+			)
 		}
 	},
 	Favorite: {
 		screen: FavoriteScreen,
 		navigationOptions: {
-			tabBarLabel: 'Fav'
+			tabBarLabel: 'Favorit',
+			tabBarIcon: ({ tintColor }) => (
+				<Icon style={{ color: tintColor }} name="ios-heart-outline" />
+			)
 		}
 	},
 	Create: {
 		screen: CreateScreen,
 		navigationOptions: {
-			tabBarLabel: 'Create'
+			tabBarLabel: 'Pasang',
+			tabBarIcon: ({ tintColor }) => (
+				<Icon style={{ color: tintColor }} name="ios-add-circle-outline" />
+			)
 		}
 	},
 	Notification: {
 		screen: NotificationScreen,
 		navigationOptions: {
-			tabBarLabel: 'Notif'
+			tabBarLabel: 'Pesan',
+			tabBarIcon: ({ tintColor }) => (
+				<Icon style={{ color: tintColor }} name="ios-notifications-outline" />
+			)
 		}
 	},
 	Profile: {
 		screen: ProfileScreen,
 		navigationOptions: {
-			tabBarLabel: 'Profile'
+			tabBarLabel: 'Profil',
+			tabBarIcon: ({ tintColor }) => (
+				<Icon style={{ color: tintColor }} name="ios-person" />
+			)
 		}
 	}
 
 }, {
 	tabBarPosition: 'bottom',
+	swipeEnabled: false,
 	tabBarOptions: {
-		labelStyle: {
-			fontSize: 10
-		},
 		activeTintColor: '#1ba39c',
-		inactiveTintColor: '#95989a',
+		inactiveTintColor: '#4d4d4d',
+		showIcon: true,
+		labelStyle: {
+			fontSize: 8,
+			paddingBottom: 0
+		},
 		style: {
 			backgroundColor: '#fff'
+		},
+		indicatorStyle: {
+			backgroundColor: '#1ba39c'
+		},
+		tabStyle: {
+			paddingBottom: 0
 		}
 	}
 });
