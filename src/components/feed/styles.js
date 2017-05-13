e528a0b1b25fb37403e9c5b57ebffbd94cee5cba
riskteria <riskteria@gmail.com>
2017-05-13 23:28:40 +0700

    fix typo

--- src/components/feed/styles.js
+++ src/components/feed/styles.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-		backgroundColor: '#97CAE5'
+		backgroundColor: colors.colorLight
 	},
 	slideText: {
 		color: '#fff',
