fe888384c2dac39ce1bdfd6b600e884b28e1b4fa
riskteria <riskteria@gmail.com>
2017-06-02 14:09:18 +0700

    search buttons grid

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import {
 	Container,
+	Content,
 	Header,
 	Body,
+	View,
 	Left,
 	Button,
 	Icon,
+	Text,
 	Title,
-	Subtitle
+	Item,
+	Subtitle,
+	InputGroup,
+	Input
 } from 'native-base';
 
 import colors from 'styles/_colors';
@@ -31,6 +37,67 @@ class SearchScreen extends Component {
 						<Subtitle onPress={() => navigate('Location')}>Select your location</Subtitle>
 					</Body>
 				</Header>
+
+				<Content>
+
+					<View style={{ padding: 16 }}>
+						<Item rounded style={{ backgroundColor: '#fff', paddingLeft: 8, paddingRight: 8 }}>
+							<Icon name="ios-search-outline" />
+							<Input placeholder="Cari sesuatu" />
+							<Icon name="md-close-circle" style={{ color: '#999' }} />
+						</Item>
+					</View>
+
+					<View style={{ flex: 1, flexDirection: 'row', paddingLeft: 8, paddingRight: 8 }}>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Cerita Kotak</Text>
+							</Button>
+						</View>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Kotak Produk</Text>
+							</Button>
+						</View>
+
+					</View>
+
+					<View style={{ flex: 1, flexDirection: 'row', paddingLeft: 8, paddingRight: 8 }}>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Kotak Bekas</Text>
+							</Button>
+						</View>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Kotak Hadiah</Text>
+							</Button>
+						</View>
+
+					</View>
+
+					<View style={{ flex: 1, flexDirection: 'row', paddingLeft: 8, paddingRight: 8 }}>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Kreasi Kotak</Text>
+							</Button>
+						</View>
+
+						<View style={{ flex: 0.5, padding: 8 }}>
+							<Button dark block style={{ borderRadius: 5 }}>
+								<Text>Daur Ulang</Text>
+							</Button>
+						</View>
+
+					</View>
+
+				</Content>
+
 			</Container>
 		);
 	}
