6befbdaedb5ebd791e72d8c4f81289217c8996fa
riskteria <riskteria@gmail.com>
2017-07-13 13:24:26 +0700

    adding splashscreen

--- src/app.android.js
+++ src/app.android.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { StyleProvider } from 'native-base';
+import SplashScreen from 'react-native-splash-screen';
 
 import App from './configs/navigators';
 import getTheme from 'theme/components';
 import platform from 'theme/variables/platform';
 
 class CarikotakApp extends Component {
+
+	componentDidMount() {
+		SplashScreen.hide();
+	}
 
 	render () {
 		return (
