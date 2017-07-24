4a5bf733366c923e17f3debd9c0061f15efb923f
riskteria <riskteria@gmail.com>
2017-07-24 13:16:58 +0700

    fetch story carousel

--- src/components/feed/CardStory.js
+++ src/components/feed/CardStory.js
@@ -1,59 +1,75 @@
 import React, { Component } from 'react';
+import { StyleSheet, Image } from 'react-native';
 import {
-	StyleSheet,
-	Image
-} from 'react-native';
-import {
-	Icon,
-	Button,
-	Card,
-	CardItem,
-	Thumbnail,
-	Text,
-	Left,
-	Body
+  Icon,
+  Button,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body
 } from 'native-base';
 
 import colors from 'styles/_colors';
 import styles from './styles';
 
 class CardStory extends Component {
+  render() {
+    const { story } = this.props;
 
-	render () {
-
-		const { item } = this.props;
-
-		return (
-			<Card style={ StyleSheet.flatten(styles.cardStoryContainer) }>
-				<Image style={ StyleSheet.flatten(styles.cardStoryImageBackground) } source={ item.image } />
-				<CardItem style={{ backgroundColor: colors.colorTransparent }}>
-					<Left>
-						<Thumbnail small circle source={item.user.picture} />
-						<Body>
-							<Text style={StyleSheet.flatten(styles.cardStoryHeaderName)}>
-								{ item.user.name }
-							</Text>
-						</Body>
-					</Left>
-				</CardItem>
-				<CardItem style={{ backgroundColor: colors.colorTransparent, flex: 1 }}>
-					<Text numberOfLines={3} style={StyleSheet.flatten(styles.cardStoryTitle)}>
-						{ item.title }
-					</Text>
-				</CardItem>
-				<CardItem style={{ backgroundColor: 'transparent', flexDirection: 'row', justifyContent: 'space-between' }}>
-					<Button light transparent small style={{ paddingLeft: 0, paddingRight: 0 }}>
-						<Icon name="ios-heart" style={{ color: colors.colorHeart }} />
-						<Text style={{ fontSize: 12 }}>200</Text>
-					</Button>
-					<Button light transparent small style={{ paddingLeft: 0, paddingRight: 0 }}>
-						<Text style={{ fontSize: 12 }}>200 Komentar</Text>
-					</Button>
-				</CardItem>
-			</Card>
-		);
-	}
-
+    return (
+      <Card style={StyleSheet.flatten(styles.cardStoryContainer)}>
+        <Image
+          style={StyleSheet.flatten(styles.cardStoryImageBackground)}
+          source={story.image}
+        />
+        <CardItem style={{ backgroundColor: colors.colorTransparent }}>
+          <Left>
+            <Thumbnail small circle source={story.user.picture} />
+            <Body>
+              <Text style={StyleSheet.flatten(styles.cardStoryHeaderName)}>
+                {story.user.name}
+              </Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem style={{ backgroundColor: colors.colorTransparent, flex: 1 }}>
+          <Text
+            numberOfLines={3}
+            style={StyleSheet.flatten(styles.cardStoryTitle)}
+          >
+            {story.title}
+          </Text>
+        </CardItem>
+        <CardItem
+          style={{
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Button
+            light
+            transparent
+            small
+            style={{ paddingLeft: 0, paddingRight: 0 }}
+          >
+            <Icon name="ios-heart" style={{ color: colors.colorHeart }} />
+            <Text style={{ fontSize: 12 }}>200</Text>
+          </Button>
+          <Button
+            light
+            transparent
+            small
+            style={{ paddingLeft: 0, paddingRight: 0 }}
+          >
+            <Text style={{ fontSize: 12 }}>200 Komentar</Text>
+          </Button>
+        </CardItem>
+      </Card>
+    );
+  }
 }
 
 export default CardStory;
--- src/components/feed/StoryCarousel.js
+++ src/components/feed/StoryCarousel.js
@@ -3,73 +3,107 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import CardStory from './CardStory';
 
-import styles, { sliderWidth, itemWidth  } from './styles';
+import { API } from 'services/APIService';
+
+import styles, { sliderWidth, itemWidth } from './styles';
 
 const cards = [
-	{
-		title: 'Lacus arcu phasellus vero pariatur, natus, taciti, sapiente animi velit, autem quod cillum explicabo at faucibus laoreet habitant exercitation tenetur commodo ullam lectus neque?',
-		name: 'One',
-		user: {
-			picture: { uri: 'https://unsplash.it/g/300x300??image=15' },
-			id: 1,
-			name: 'Axel Rose'
-		},
-		image: { uri: 'https://unsplash.it/300x300??image=10' }
-	},{
-		title: 'Ut commodi quaerat! Fugiat laoreet rem ex metus deserunt netus volutpat penatibus fuga placeat itaque, cubilia, totam orci?',
-		name: 'Two',
-		user: {
-			picture: { uri: 'https://unsplash.it/g/300x300??image=19' },
-			id: 2,
-			name: 'Alexis Sanchez'
-		},
-		image: { uri: 'https://unsplash.it/300x300?image=50' }
-	},
+  {
+    title:
+      'Lacus arcu phasellus vero pariatur, natus, taciti, sapiente animi velit, autem quod cillum explicabo at faucibus laoreet habitant exercitation tenetur commodo ullam lectus neque?',
+    name: 'One',
+    user: {
+      picture: { uri: 'https://unsplash.it/g/300x300??image=15' },
+      id: 1,
+      name: 'Axel Rose'
+    },
+    image: { uri: 'https://unsplash.it/300x300??image=10' }
+  },
+  {
+    title:
+      'Ut commodi quaerat! Fugiat laoreet rem ex metus deserunt netus volutpat penatibus fuga placeat itaque, cubilia, totam orci?',
+    name: 'Two',
+    user: {
+      picture: { uri: 'https://unsplash.it/g/300x300??image=19' },
+      id: 2,
+      name: 'Alexis Sanchez'
+    },
+    image: { uri: 'https://unsplash.it/300x300?image=50' }
+  }
 ];
 
 class StoryCarousel extends Component {
+  constructor(props) {
+    super(props);
 
-	render () {
+    this._getStories = this._getStories.bind(this);
 
-		const navigate = this.props.navigate;
+    this.state = {
+      stories: []
+    };
+  }
 
-		const CardSwipe = cards.map((item, index) => (
-			<TouchableOpacity
-				key={index}
-				activeOpacity={0.8}
-				style={styles.slideInnerContainer}
-				onPress={() => navigate('Story')}>
-				<CardStory key={index} item={item} />
-			</TouchableOpacity>
-		));
+  _getStories() {
+    console.log('getStories');
+    API.get('api/post?page=1&&per_page=5')
+      .then(res => {
+        this.setState({ stories: res.data });
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
 
-		return (
-			<View>
+  componentWillMount() {
+    this._getStories();
+  }
 
-				<View style={styles.tabProductSectionTop}>
-					<Text style={styles.tabProductSectionLabel}>Cerita Terpopuler</Text>
-					<Text onPress={() => navigate('StoryList')} style={styles.tabProductSectionLink}>Lihat Semua</Text>
-				</View>
+  render() {
+    const navigate = this.props.navigate;
 
-				<Carousel
-					sliderWidth={sliderWidth}
-					itemWidth={itemWidth}
-					firstItem={1}
-					inactiveSlideScale={0.94}
-					inactiveSlideOpacity={0.6}
-					enableMomentum={false}
-					containerCustomStyle={styles.slider}
-					contentContainerCustomStyle={styles.sliderContainer}
-					showsHorizontalScrollIndicator={false}
-					snapOnAndroid={true}
-					removeClippedSubviews={false}>
-						{ CardSwipe }
-				</Carousel>
+    const CardSwipe = cards.map((story, index) =>
+      <TouchableOpacity
+        key={index}
+        activeOpacity={0.8}
+        style={styles.slideInnerContainer}
+        onPress={() => navigate('Story')}
+      >
+        <CardStory key={index} story={story} />
+      </TouchableOpacity>
+    );
 
-			</View>
-		);
-	}
+    const CarouselStory = () =>
+      <Carousel
+        sliderWidth={sliderWidth}
+        itemWidth={itemWidth}
+        firstItem={1}
+        inactiveSlideScale={0.94}
+        inactiveSlideOpacity={0.6}
+        enableMomentum={false}
+        containerCustomStyle={styles.slider}
+        contentContainerCustomStyle={styles.sliderContainer}
+        showsHorizontalScrollIndicator={false}
+        snapOnAndroid={true}
+        removeClippedSubviews={false}
+      >
+        {CardSwipe}
+      </Carousel>;
 
+    return (
+      <View>
+        <View style={styles.tabProductSectionTop}>
+          <Text style={styles.tabProductSectionLabel}>Cerita Terpopuler</Text>
+          <Text
+            onPress={() => navigate('StoryList')}
+            style={styles.tabProductSectionLink}
+          >
+            Lihat Semua
+          </Text>
+        </View>
+        <CarouselStory />
+      </View>
+    );
+  }
 }
 
 export default StoryCarousel;
