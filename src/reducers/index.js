8417944e4b30b64dcabb115b220471893eabc6e6
riskteria <riskteria@gmail.com>
2017-07-25 13:47:33 +0700

    remove useless reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,6 @@ import { combineReducers } from 'redux';
 import { RootNavigator } from 'navigators/rootNavigator/RootNavigator';
 import { MainNavigatorAuth } from 'navigators/mainNavigatorAuth/MainNavigatorAuth';
 import { MainNavigatorNoAuth } from 'navigators/mainNavigatorNoAuth/MainNavigatorNoAuth';
-import { MainTabReducer } from 'navigators/mainTab/MainTabNavigator';
-import { FeedTabNavigator } from 'navigators/tabFeed/FeedTabNavigator';
-import { FavoriteTabNavigator } from 'navigators/tabFavorite/FavoriteTabNavigator';
-import { CreateTabNavigator } from 'navigators/tabCreate/CreateTabNavigator';
-import { MessageTabNavigator } from 'navigators/tabMessage/MessageTabNavigator';
-import { ProfileTabNavigator } from 'navigators/tabProfile/ProfileTabNavigator';
 
 const reducers = combineReducers({
   rootNavigator: (state, action) =>
@@ -18,24 +12,7 @@ const reducers = combineReducers({
     MainNavigatorAuth.router.getStateForAction(action, state),
 
   mainNavigatorNoAuth: (state, action) =>
-    MainNavigatorNoAuth.router.getStateForAction(action, state),
-
-  mainTabNavigator: MainTabReducer,
-
-  feedTabNavigator: (state, action) =>
-    FeedTabNavigator.router.getStateForAction(action, state),
-
-  favoriteTabNavigator: (state, action) =>
-    FavoriteTabNavigator.router.getStateForAction(action, state),
-
-  createTabNavigator: (state, action) =>
-    CreateTabNavigator.router.getStateForAction(action, state),
-
-  messageTabNavigator: (state, action) =>
-    MessageTabNavigator.router.getStateForAction(action, state),
-
-  profileTabNavigator: (state, action) =>
-    ProfileTabNavigator.router.getStateForAction(action, state)
+    MainNavigatorNoAuth.router.getStateForAction(action, state)
 });
 
 export default reducers;
