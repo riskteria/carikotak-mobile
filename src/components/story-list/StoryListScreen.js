284712ccee20cf911bee361eba99985571c0b510
riskteria <riskteria@gmail.com>
2017-07-25 10:44:50 +0700

    fetch stories

--- src/components/story-list/StoryListScreen.js
+++ src/components/story-list/StoryListScreen.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Container, Text } from 'native-base';
 
+import { API } from 'services/APIService';
+
+import { startLoadingSpin, stopLoadingSpin } from 'actions/spinnerAction';
+import ProgressBarContainer from 'components/_shared/progress-bar/ProgressBarContainer';
+
+const mapStateToProps = state => {
+  return {
+    loadingSpin: state.loadingSpin
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    startSpin: () => {
+      dispatch(startLoadingSpin());
+    },
+    stopSpin: () => {
+      dispatch(stopLoadingSpin());
+    }
+  };
+};
+
 class StoryListScreen extends Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props);
-	}
+    this._onGetStories = this._onGetStories.bind(this);
 
-	render() {
-		return (
-			<Container>
-				<Text>This is Story List Screen</Text>
-			</Container>
-		);
-	}
+    this.state = {
+      stories: []
+    };
+  }
 
+  _onGetStories() {
+    const { startSpin, stopSpin } = this.props;
+
+    startSpin();
+
+    API.get('api/post')
+      .then(res => {
+        stopSpin();
+        this.setState({ stories: res.data });
+      })
+      .catch(err => {
+        stopSpin();
+        throw err;
+      });
+  }
+
+  componentWillMount() {
+    this._onGetStories();
+  }
+
+  render() {
+    const { loadingSpin } = this.props;
+
+    const StoryList = () =>
+      <Text>
+        This is Story List, total: {this.state.stories.length}
+      </Text>;
+
+    return (
+      <Container>
+        {loadingSpin.show ? <ProgressBarContainer /> : <StoryList />}
+      </Container>
+    );
+  }
 }
 
-export default StoryListScreen;
-
+export default connect(mapStateToProps, mapDispatchToProps)(StoryListScreen);
