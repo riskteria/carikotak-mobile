aa3a5a94ae4fe1c9ec1011bd75f90d95eb46f49b
riskteria <riskteria@gmail.com>
2017-07-28 12:24:43 +0700

    location selector

--- src/components/search/SearchScreenHeader.js
+++ src/components/search/SearchScreenHeader.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-import { Header, Body, Left, Button, Icon, Title, Subtitle } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Header, Body, Left, Button, Icon, Text } from 'native-base';
 
+import SearchScreenHeaderLocation from './SearchScreenHeaderLocation';
 import colors from 'styles/_colors';
 
 class SearchScreenHeader extends Component {
+  constructor(props) {
+    super(props);
+
+    this._onModalLocationCloseRequested = this._onModalLocationCloseRequested.bind(
+      this
+    );
+    this._onModalLocationOpenRequested = this._onModalLocationOpenRequested.bind(
+      this
+    );
+
+    this._onLocationChanged = this._onLocationChanged.bind(this);
+
+    this.state = {
+      modalLocationVisible: false,
+      province: {
+        id: '',
+        name: ''
+      },
+      city: {
+        id: '',
+        name: ''
+      }
+    };
+  }
+
+  _onLocationChanged(province, city) {
+    this.setState({ province, city });
+    this._onModalLocationCloseRequested();
+  }
+
+  _onModalLocationCloseRequested() {
+    this.setState({ modalLocationVisible: false });
+  }
+
+  _onModalLocationOpenRequested() {
+    this.setState({ modalLocationVisible: true });
+  }
+
   render() {
-    const { goBack, navigate } = this.props.navigation;
+    const { modalLocationVisible, province, city } = this.state;
+    const { navigation } = this.props;
+    const { goBack } = navigation;
 
     return (
       <Header style={{ backgroundColor: colors.colorLight, elevation: 1 }}>
@@ -15,16 +57,26 @@ class SearchScreenHeader extends Component {
           </Button>
         </Left>
         <Body>
-          <Title
-            onPress={() => navigate('Location')}
-            style={{ color: colors.colorDark }}
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => this._onModalLocationOpenRequested()}
           >
-            Your location
-          </Title>
-          <Subtitle onPress={() => navigate('Location')}>
-            Select your location
-          </Subtitle>
+            <Text style={{ color: colors.colorDark }}>Lokasi anda</Text>
+            <Text note>
+              {!province.name && !city.name
+                ? 'Pilih Lokasi Anda'
+                : (province.name && city.name
+                    ? province.name + ', '
+                    : province.name) + city.name}
+            </Text>
+          </TouchableOpacity>
         </Body>
+
+        <SearchScreenHeaderLocation
+          modalLocationVisible={modalLocationVisible}
+          _onLocationChanged={this._onLocationChanged}
+          _onModalLocationCloseRequested={this._onModalLocationCloseRequested}
+        />
       </Header>
     );
   }
---
+++ src/components/search/SearchScreenHeaderLocation.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import { Modal, Picker } from 'react-native';
+import {
+  Header,
+  Left,
+  Button,
+  Icon,
+  Body,
+  Text,
+  Container,
+  Content
+} from 'native-base';
+
+import ProgressBar from 'components/_shared/progress-bar/ProgressBar';
+import colors from 'styles/_colors';
+
+import { API } from 'services/APIService';
+
+class SearchScreenHeaderLocation extends Component {
+  constructor(props) {
+    super(props);
+
+    this._onCityValueChanged = this._onCityValueChanged.bind(this);
+    this._onProvinceValueChanged = this._onProvinceValueChanged.bind(this);
+    this._fetchAllLocation = this._fetchAllLocation.bind(this);
+
+    this.state = {
+      selectedProvince: {
+        id: '',
+        name: ''
+      },
+      selectedCity: {
+        name: '',
+        id: ''
+      },
+      loadingSpin: false,
+      locations: [],
+      cities: []
+    };
+  }
+
+  _onCityValueChanged(city, index) {
+    const selectedCity = this.state.cities.find(c => c.id === city);
+
+    this.setState({
+      selectedCity: { id: selectedCity.id, name: selectedCity.name }
+    });
+  }
+
+  _onProvinceValueChanged(province, index) {
+    const selectedProvince = this.state.locations.find(
+      location => location.id === province
+    );
+    this.setState({
+      selectedProvince: { id: selectedProvince.id, name: selectedProvince.name }
+    });
+    this.setState({ cities: selectedProvince.cities });
+  }
+
+  _fetchAllLocation() {
+    this.setState({ loadingSpin: true });
+    API.get('api/location?get=province&with=cities')
+      .then(res => {
+        this.setState({ loadingSpin: false, locations: res.data });
+      })
+      .catch(err => {
+        this.setState({ loadingSpin: false });
+        throw err;
+      });
+  }
+
+  componentWillMount() {
+    this._fetchAllLocation();
+  }
+
+  render() {
+    const {
+      locations,
+      cities,
+      loadingSpin,
+      selectedProvince,
+      selectedCity
+    } = this.state;
+
+    const {
+      modalLocationVisible,
+      _onModalLocationCloseRequested,
+      _onLocationChanged
+    } = this.props;
+
+    const ProvincePicker = () =>
+      <Picker
+        selectedValue={selectedProvince.id}
+        onValueChange={this._onProvinceValueChanged}
+      >
+        <Picker.Item value="" label="Select your province" />
+        {ProvinceItemPicker()}
+      </Picker>;
+
+    const ProvinceItemPicker = () =>
+      locations.map((province, index) =>
+        <Picker.Item label={province.name} value={province.id} key={index} />
+      );
+
+    const CityPicker = () =>
+      <Picker
+        selectedValue={selectedCity.id}
+        onValueChange={this._onCityValueChanged}
+      >
+        <Picker.Item value="" label="Select your city" />
+        {CityItemPicker()}
+      </Picker>;
+
+    const CityItemPicker = () =>
+      cities.map((city, index) =>
+        <Picker.Item label={city.name} value={city.id} key={index} />
+      );
+
+    const LocationPicker = () =>
+      <Content style={{ padding: 16 }}>
+        <ProvincePicker />
+        <CityPicker />
+        <Button
+          block
+          onPress={() => _onLocationChanged(selectedProvince, selectedCity)}
+        >
+          <Text>Change</Text>
+        </Button>
+      </Content>;
+
+    return (
+      <Modal
+        animationType={'slide'}
+        transparent={false}
+        visible={modalLocationVisible}
+        onRequestClose={() => _onModalLocationCloseRequested()}
+      >
+        <Header style={{ backgroundColor: colors.colorLight, elevation: 1 }}>
+          <Left style={{ flex: 0.16 }}>
+            <Button
+              transparent
+              dark
+              onPress={() => _onModalLocationCloseRequested()}
+            >
+              <Icon name="md-close" />
+            </Button>
+          </Left>
+          <Body>
+            <Text>Select your location</Text>
+          </Body>
+        </Header>
+        <Container>
+          {loadingSpin ? <ProgressBar /> : <LocationPicker />}
+        </Container>
+      </Modal>
+    );
+  }
+}
+
+export default SearchScreenHeaderLocation;
