eb6b0122f50ce1915146b6baa184e4a783b2a707
riskteria <riskteria@gmail.com>
2017-08-02 06:03:52 +0700

    fix my box temporary

--- src/components/feed/FeedFindMyBox.js
+++ src/components/feed/FeedFindMyBox.js
@@ -6,28 +6,34 @@ import { NavigationActions } from 'react-navigation';
 import styles from './styles';
 
 class FeedFindMyBox extends Component {
+  render() {
+    const navigate = this.props.navigate;
 
-	render () {
-
-		const navigate = this.props.navigate;
-
-		return (
-			<View style={ styles.sectionFindMyBox }>
-				<Button
-					iconLeft iconRight full large rounded bordered dark
-					style={{ borderColor: '#c4c4c4' }}
-					onPress={() => navigate('Find')}>
-					<Icon name="md-search" style={{ color: '#c4c4c4' }} />
-					<View style={styles.buttonFindTextInside}>
-						<Text style={ styles.buttonFindTextTitle }>Temukan kotak</Text>
-						<Text style={ styles.buttonFindTextSubtitle }>Temukan kotak kebutuhan anda</Text>
-					</View>
-					<Icon name="ios-arrow-forward" style={{ color: '#c4c4c4' }} />
-				</Button>
-			</View>
-		);
-	}
-
+    return (
+      <View style={styles.sectionFindMyBox}>
+        <Button
+          iconLeft
+          iconRight
+          full
+          large
+          rounded
+          bordered
+          dark
+          style={{ borderColor: '#c4c4c4' }}
+          onPress={() => navigate('ProductList')}
+        >
+          <Icon name="md-search" style={{ color: '#c4c4c4' }} />
+          <View style={styles.buttonFindTextInside}>
+            <Text style={styles.buttonFindTextTitle}>Temukan kotak</Text>
+            <Text style={styles.buttonFindTextSubtitle}>
+              Temukan kotak kebutuhan anda
+            </Text>
+          </View>
+          <Icon name="ios-arrow-forward" style={{ color: '#c4c4c4' }} />
+        </Button>
+      </View>
+    );
+  }
 }
 
 export default FeedFindMyBox;
